27a3c7a8d7258678974ffcdb63ecef729c1e7a7c
Paradox68 <paradoxscape@live.com>
2015-05-22 15:00:42 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -2476,19 +2476,18 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                    	if (user.username === 'Sode Summer シ') {
-                    	API.sendChat(subChat("Sode Summer is a total faggot.")); return void (0);
-                      } else {
+
                     	
                         var msg = chat.message;
                         if (msg.length <= cmd.length + 1) return API.sendChat('/me MotD: ' + basicBot.settings.motd);
                         var argument = msg.substring(cmd.length + 1);
                         if (!basicBot.settings.motdEnabled) basicBot.settings.motdEnabled = !basicBot.settings.motdEnabled;
                         if (isNaN(argument)) {
+                        	if (!user.username === 'Sode Summer シ') {
                             basicBot.settings.motd = argument;
                             API.sendChat(subChat(basicBot.chat.motdset, {msg: basicBot.settings.motd}));
+                        	}
                         }
-                      }
                         else {
                             basicBot.settings.motdInterval = argument;
                             API.sendChat(subChat(basicBot.chat.motdintervalset, {interval: basicBot.settings.motdInterval}));
