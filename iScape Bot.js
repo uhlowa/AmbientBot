f6b9ef6de112ec815c1d17afbeb8cd0a2e8c1630
Paradox68 <paradoxscape@live.com>
2015-05-22 15:36:25 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -2470,7 +2470,7 @@
 
             motdCommand: {
                 command: 'motd',
-                rank: 'bouncer',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
