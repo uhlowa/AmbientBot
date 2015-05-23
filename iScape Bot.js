ea52c4a0980d3c99d25765740e6b5272b087cecd
Paradox68 <paradoxscape@live.com>
2015-05-23 16:39:48 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -240,7 +240,7 @@
             website: "http://www.the-underground.info/",
             intervalMessages: [],
             messageInterval: 5,
-            songstats: false,
+            songstats: true,
             commandLiteral: "!",
             blacklists: {
                 NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
@@ -836,7 +836,7 @@
             if (typeof lastplay === 'undefined') return;
             if (basicBot.settings.songstats) {
                 if (typeof basicBot.chat.songstatistics === "undefined") {
-                    API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + "W/" + lastplay.score.grabs + "G/" + lastplay.score.negative + "M.")
+                    API.sendChat("/me Last Play: \n" + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + " woots -" + lastplay.score.grabs + " grabs -" + lastplay.score.negative + " Mehs.")
                 }
                 else {
                     API.sendChat(subChat(basicBot.chat.songstatistics, {artist: lastplay.media.author, title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
