c9c1f826a4df4fd33d441bdaa22c5d802e98a9e9
Paradox68 <paradoxscape@live.com>
2015-05-22 23:31:44 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -2022,7 +2022,7 @@
                                     )
                             }
                             var api_key = "dc6zaTOxFJmzC"; // public beta key
-                            var rating = "pg-13"; // PG 13 gifs
+                            var rating = "R"; // PG 13 gifs
                             var tag = msg.substr(cmd.length + 1);
                             var fixedtag = tag.replace(/ /g,"+");
                             var commatag = tag.replace(/ /g,", ");
@@ -2051,7 +2051,7 @@
                                     )
                             }
                             var api_key = "dc6zaTOxFJmzC"; // public beta key
-                            var rating = "pg-13"; // PG 13 gifs
+                            var rating = "R"; // PG 13 gifs
                             get_random_id(api_key, function(id) {
                                 if (typeof id !== 'undefined') {
                                     API.sendChat(subChat(basicBot.chat.validgifrandom, {name: chat.un, id: id}));
