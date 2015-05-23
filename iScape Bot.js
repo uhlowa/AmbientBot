754922f99ebdec3269ffb22fda371cb40009dacb
Paradox68 <paradoxscape@live.com>
2015-05-22 21:58:55 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -2210,7 +2210,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+                        $.get("https://rawgit.com/Paradox68/iScape-Bot/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
