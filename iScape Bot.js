53210b8b82c1e96f39f47c176ee4807bfae54ef1
Paradox68 <paradoxscape@live.com>
2015-05-23 14:10:52 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -3813,6 +3813,58 @@
                     	API.sendChat(subChat('http://i.imgur.com/d61Aszu.webm', {link: 'http://i.imgur.com/d61Aszu.webm'}));
             	}
             },
+            
+            approveCommand: {
+            	command: 'approve',
+                rank: 'bouncer',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var name;
+                        if (msg.length === cmd.length) name = chat.un;
+                        else {
+                            name = msg.substr(cmd.length + 2);
+                        }
+                        users = API.getUsers();
+                        var len = users.length;
+                        for (var i = 0; i < len; ++i){
+                            if (users[i].username == name){
+                            	PI.sendChat(subChat('/me ' + users[i].username + '\'s next song has been approved!'));
+                            }
+                        }
+                    }
+                }
+                            	
+            }
+            
+                        opinionCommand: {
+            	command: 'opinion',
+                rank: 'bouncer',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var name;
+                        if (msg.length === cmd.length) name = chat.un;
+                        else {
+                            name = msg.substr(cmd.length + 2);
+                        }
+                        users = API.getUsers();
+                        var len = users.length;
+                        for (var i = 0; i < len; ++i){
+                            if (users[i].username == name){
+                            	PI.sendChat(subChat('/me ' + users[i].username + '\'s opinion no longer means shit!'));
+                            }
+                        }
+                    }
+                }
+                            	
+            }
 
 
             whoisCommand: {
