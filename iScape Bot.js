0268e961f6b67d7933c5e0cfd56dcc87291261c4
Paradox68 <paradoxscape@live.com>
2015-05-23 15:04:11 -0400

    Update iScape Bot.js

--- iScape Bot.js
+++ iScape Bot.js
@@ -304,10 +304,7 @@
                     var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
                     var winner = basicBot.room.roulette.participants[ind];
                     basicBot.room.roulette.participants = [];
-                    var pos = API.getWaitListPosition() - 3;
-                    if (pos < 1) {
-                    		pos = 1;
-                    }
+                    var pos = 2;
                     //var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
                     var user = basicBot.userUtilities.lookupUser(winner);
                     var name = user.username;
