d5bdba6038eba9b685739f477902aff0aed101c3
Paradox68 <paradoxscape@live.com>
2015-05-25 03:36:17 -0400

    Update UndergroundBot.js

--- UndergroundBot.js
+++ UndergroundBot.js
@@ -2894,20 +2894,7 @@
                 }
             },
             
-                        russianCommand: {
-                command: 'roulette',
-                rank: 'mod',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!underground.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        if (!underground.room.russiangame.RRStatus) {
-                            underground.room.russiangame.startRussianGame();
-                        }
-                    }
-                }
-            },
+
             
             dicegameCommand: {
                 command: 'dicegame',
