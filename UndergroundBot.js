f91e5a673891143007bc47baab5a3a7c39cac454
Tommy <tommys212@gmail.com>
2015-05-27 13:57:49 -0400

    another string fix...

--- UndergroundBot.js
+++ UndergroundBot.js
@@ -3603,7 +3603,7 @@
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) { return void (0); }
-                    API.sendChat(subChat('The open source code for ' + underground.settings.botName +  is available at: https://github.com/Paradox68/UndergroundBot'));
+                    API.sendChat(subChat('The open source code for ' + underground.settings.botName +  'is available at: https://github.com/Paradox68/UndergroundBot'));
                         }
                 },
                 dance6Command: {
