dd01d79672289e9b93eeb5ddfdf2845b694987ed
Paradox68 <paradoxscape@live.com>
2015-05-28 22:32:15 -0400

    Update UndergroundBot.js

--- UndergroundBot.js
+++ UndergroundBot.js
@@ -184,7 +184,7 @@
     var botCreatorIDs = ["3995934", "4105209"];
 
     var underground = {
-        version: "1.8.3",
+        version: "1.8.4",
         status: false,
         name: "Underground Bot",
         loggedInID: null,
