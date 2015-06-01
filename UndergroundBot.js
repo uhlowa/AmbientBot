bcdadc2dfab90576ec1eacf118de778a7b3ab656
Paradox68 <paradoxscape@live.com>
2015-06-01 17:20:48 -0400

    Update UndergroundBot.js

--- UndergroundBot.js
+++ UndergroundBot.js
@@ -3008,7 +3008,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!underground.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('The ' + underground.settings.botName +  'was developed and is operated by Paradox VII');
+                        API.sendChat('The ' + underground.settings.botName +  ' was developed and is operated by Paradox VII');
                     }
                 }
             },
